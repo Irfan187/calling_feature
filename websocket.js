0f98fda6deee6fe8d3aa35e226b3b990b188ebf8
Irfan Elahi <37905604+Irfan187@users.noreply.github.com>
2024-07-26 19:02:50 +0500

    change websocket config

--- websocket.js
+++ websocket.js
@@ -3,6 +3,7 @@ import { WebSocketServer } from 'ws';
 import http from 'http';
 
 const server = http.createServer();
+console.log(server);
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', function connection(ws) {
