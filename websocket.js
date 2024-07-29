05aa6e78e7154e344bbf2d1424e9d1113873bd1f
Irfan Elahi <37905604+Irfan187@users.noreply.github.com>
2024-07-29 18:44:52 +0500

    change websocket config

--- websocket.js
+++ websocket.js
@@ -1,42 +1,23 @@
-// import WebSocket from 'ws';
+import WebSocket from 'ws';
 
-// const ws = new WebSocket('ws://callingfeature.scrumad.com:5000');
+const ws = new WebSocket('ws://callingfeature.scrumad.com:5000');
 
-// ws.on('open', function open() {
-//   console.log('connected');
-//   ws.send('Welcome to the WebSocket server!');
-//   console.log('Welcome to the WebSocket server!');
-// });
-
-// ws.on('close', function close() {
-//   console.log('disconnected');
-// });
-
-// ws.on('message', function message(data) {
-//   console.log('received: %s', data);
-// });
-// ws.on('error', function error(err) {
-//   console.error('WebSocket error:', err);
-// });
-// ws.on("listening", () => {
-//   console.log("Server running at port 5000 is listening");
-// });
-import { createServer } from 'http';
-import { WebSocketServer } from 'ws';
-
-const server = createServer();
-const wss = new WebSocketServer({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-
-  ws.on('message', (message) => {
-    console.log(`Received message: ${message}`);
-  });
-
-  ws.send('Hello from the WebSocket server');
+ws.on('open', function open() {
+  console.log('connected');
+  ws.send('Welcome to the WebSocket server!');
+  console.log('Welcome to the WebSocket server!');
 });
 
-server.listen(5000, () => {
-  console.log('WebSocket server is listening on port 5000');
+ws.on('close', function close() {
+  console.log('disconnected');
+});
+
+ws.on('message', function message(data) {
+  console.log('received: %s', data);
+});
+ws.on('error', function error(err) {
+  console.error('WebSocket error:', err);
+});
+ws.on("listening", () => {
+  console.log("Server running at port 5000 is listening");
 });
