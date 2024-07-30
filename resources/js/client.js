fb69521ccbfc9ad66595948c32023e285d8dffb3
Irfan Elahi <37905604+Irfan187@users.noreply.github.com>
2024-07-30 18:15:20 +0500

    change websocket config

--- resources/js/client.js
+++ resources/js/client.js
@@ -1,25 +1,23 @@
-// import WebSocket from 'ws';
-
 const ws = new WebSocket('wss://callingfeature.scrumad.com:3001');
 
 // ws.onerror((error) => {
 //     console.log(error);
 // });
 
-ws.onopen((event) => {
+ws.onopen = (event) => {
   console.log('connected');
   ws.send('dsadsfdsfdfs');
-});
+};
 
-ws.onclose((event) => {
+ws.onclose = (event) => {
   console.log('disconnected');
-});
+};
 
-ws.onmessage((event) => {
+ws.onmessage = (event) => {
   console.log(event);
 
   ws.send('dtrrtr');
-});
+};
 
 
 export { ws }
