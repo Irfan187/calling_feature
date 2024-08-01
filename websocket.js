a26d258fd50e476788c847ea6a103daa769956fc
Irfan Elahi <37905604+Irfan187@users.noreply.github.com>
2024-08-01 11:28:06 +0500

    change websocket config

--- websocket.js
+++ websocket.js
@@ -1,40 +1,9 @@
-// import https from "https";
-// import fs from "fs";
-// import { WebSocketServer } from "ws";
-
-// const privateKey = fs.readFileSync("/etc/nginx/ssl/callingfeature.scrumad.com/2279529/server.key", "utf8");
-// const certificate = fs.readFileSync("/etc/nginx/ssl/callingfeature.scrumad.com/2279529/server.crt", "utf8");
-// const credentials = { key: privateKey, cert: certificate };
-
-// const httpsServer = https.createServer(credentials);
-// const wss = new WebSocketServer({ server: httpsServer, clientTracking: true, });
-
-// wss.on("connection", (ws) => {
-//     ws.on("message", (message) => {
-//         console.log('Received message:', message);
-//     });
-
-//     ws.on("open", (message) => {
-//         console.log('WebSocket open',message);
-//     });
-
-//     ws.on("close", (code, reason) => {
-//         console.log(`WebSocket closed: code=${code}, reason=${reason}`);
-//     });
-
-//     ws.send("Welcome to the WebSocket server!");
-// });
-
-// httpsServer.listen(3001, () => {
-//     console.log("Server running at port 3001");
-// });
-
-// wss.on("listening", () => {
-//     console.log("Server running at port 3001 is listening");
-// });
 import { createServer } from 'https';
 import { readFileSync } from 'fs';
 import { server as WebSocketServer } from 'websocket';
+const Opus = require('node-opus');
+const decoder = new Opus.Decoder(48000, 1); // 48kHz sampling rate, mono channel
+
 
 var httpsOptions = {
   key: readFileSync("/etc/nginx/ssl/callingfeature.scrumad.com/2279529/server.key"),
@@ -82,12 +51,13 @@ wsServer.on('request', function (request) {
     //   console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
     //   connection.sendBytes(message.binaryData);
     // }
-    if (event.data instanceof ArrayBuffer) {
-      console.log('Received binary data:', event.data);
-      // Handle ArrayBuffer data
-    } else {
-      console.log('Received text data:', event.data);
-    }
+    const decodedData = decoder.decode(message);
+        // Process the decoded audio data or relay it to other clients
+        wsServer.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(decodedData);
+            }
+        });
   });
 
   connection.on('close', function (reasonCode, description) {
