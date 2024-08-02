d11fb0f452825be97e6e0f11722d62e877c28e87
Irfan Elahi <37905604+Irfan187@users.noreply.github.com>
2024-08-02 14:07:52 +0500

    change websocket config

--- websocket.js
+++ websocket.js
@@ -30,7 +30,7 @@ function originIsAllowed(origin) {
   // put logic here to detect whether the specified origin is allowed.
   return true;
 }
-
+var window = new Window;
 function processPayload(payload) {
   const binaryString = window.atob(payload);
   const len = binaryString.length;
@@ -41,7 +41,7 @@ function processPayload(payload) {
   for (let i = 0; i < len; i++) {
     bytes[i] = binaryString.charCodeAt(i);
   }
-  const blob = new Blob([bytes], { type: 'audio/wav' });
+  const blob = new Blob([bytes], { type: 'audio/mp3' });
   const url = URL.createObjectURL(blob);
 
   const audio = new Audio(url);
